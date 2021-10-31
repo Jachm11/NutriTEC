6a9a359a5bbda08c0b2e2d7004a8e59d329b16b7
Heutlett <44324449+Heutlett@users.noreply.github.com>
2021-10-31 15:00:24 -0600

    Update script db

--- scripts/db.sql
+++ scripts/db.sql
@@ -5,14 +5,6 @@ WHERE  TABLE_TYPE = 'BASE TABLE'
 Exec Sp_executesql @sql
 
 
-CREATE TABLE Employees (
-    id int NOT NULL PRIMARY KEY,
-    username varchar(12) NOT NULL,
-	password varchar(8) NOT NULL,
-	birthdate Date NOT NULL
-);
-
-
 CREATE TABLE Clientes (
     id					int NOT NULL,
 	id_nutricionista	int NOT NULL,
@@ -154,16 +146,3 @@ CREATE TABLE Consumo_diario(
 
 	PRIMARY KEY(id_cliente,id_producto)
 );
-
-
-INSERT INTO Employees (id, username, password, birthdate)
-		VALUES ('1', 'Shak', '123', '10/21/2000');
-
-INSERT INTO Employees (id, username, password, birthdate) 
-		VALUES ('2', 'Adrian', '123', '06/21/1999');
-
-INSERT INTO Employees (id, username, password, birthdate)
-		VALUES ('3', 'Jose', '123', '09/15/2001');
-
-INSERT INTO Employees (id, username, password, birthdate)
-		VALUES ('4', 'Sebas', '123', '12/31/2000');
