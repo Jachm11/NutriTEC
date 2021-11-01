c127cd9fc57ba8ee9bcd5e66fba50514b2e9fdda
Heutlett <44324449+Heutlett@users.noreply.github.com>
2021-10-31 22:02:59 -0600

    Update to scripts

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,10 +1,3 @@
-DECLARE @sql NVARCHAR(max)=''
-SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
-FROM   INFORMATION_SCHEMA.TABLES
-WHERE  TABLE_TYPE = 'BASE TABLE'
-Exec Sp_executesql @sql
-
-
 CREATE TABLE Cliente (
     id					int NOT NULL,
 	id_nutricionista	int NOT NULL,
