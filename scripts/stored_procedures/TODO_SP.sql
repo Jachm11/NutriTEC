008eacc00bb876de4c7c9c6393601631b1e307d6
Heutlett <44324449+Heutlett@users.noreply.github.com>
2021-11-07 04:06:30 -0600

    Modified. Se actualiza la guia del API

--- scripts/stored_procedures/TODO_SP.sql
+++ scripts/stored_procedures/TODO_SP.sql
@@ -1,6 +1,84 @@
+----------------------------- HASH MD5 -------------------------------------
+USE [nutridb]
+IF OBJECT_ID('Hash_MD5', 'P') IS NOT NULL
+    DROP PROCEDURE [Hash_MD5];
+GO
+
+CREATE FUNCTION dbo.[Hash_MD5](
+    @data varchar(max)
+)
+    RETURNS VARCHAR(max)
+AS
+BEGIN
+    DECLARE @hash VARCHAR(max)
+    SELECT @hash = CONVERT(VARCHAR(max), HashBytes('MD5', @data), 2)
+    RETURN @hash
+END
+
+GO
+
+---------------------------------------- TRIGER MD5 -------------------------------------------------------
+
+IF OBJECT_ID('MD5', 'P') IS NOT NULL
+    DROP TRIGGER [MD5];
+GO
+
+-- TRIGGER MD5
+CREATE TRIGGER dbo.[MD5]
+    ON dbo.Usuario
+    AFTER INSERT, UPDATE
+    AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE
+        @id int,
+        @clave VARCHAR(max),
+        @md5 VARCHAR(max)
+
+    -- id
+    SELECT @id = id FROM INSERTED
+    -- clave
+    SELECT @clave = clave FROM INSERTED
+    -- MD5
+    SET @md5 = (SELECT dbo.Hash_MD5(@clave))
+
+    UPDATE Usuario
+    SET clave = @md5
+    WHERE id = @id;
+
+END
+---------------------------------------- RECIPE RELATIONS TRIGGER -----------------------------------------
+
+IF OBJECT_ID('Products_Recipe', 'P') IS NOT NULL
+    DROP TRIGGER [RecipeRelations];
+GO
+
+-- TRIGGER MD5
+CREATE TRIGGER dbo.[RecipeRelations]
+    ON dbo.Receta
+    AFTER UPDATE
+    AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE
+        @id int,
+        @status VARCHAR(max)
+
+    -- id
+    SELECT @id = id FROM DELETED
+    -- status
+    SELECT @status = estatus FROM INSERTED
+
+    if @status = 'INACTIVO'
+        BEGIN
+            DELETE FROM Producto_receta
+            WHERE id_receta = @id
+        END
+END
 
 -------------------------------------------- UNQUIE FECHA MEDIDA -------------------------------------------
-use nutridb;
 
 IF OBJECT_ID ( 'UniqueFechaMedida', 'P' ) IS NOT NULL
     DROP PROCEDURE UniqueFechaMedida;
@@ -42,10 +120,9 @@ Create procedure [dbo].UniqueFechaMedida
 GO
 
 --------------------------------------------- UNIQUE BARCODE ---------------------------------------------
-use nutridb;
 
 IF OBJECT_ID ( 'UniqueBarcode', 'P' ) IS NOT NULL
-    DROP PROCEDURE UniqueEmail;
+    DROP PROCEDURE UniqueBarcode;
 GO
 
 Create procedure [dbo].UniqueBarcode
@@ -81,9 +158,7 @@ Create procedure [dbo].UniqueBarcode
 	END
 
 GO
-
 ------------------------------------------ UNIQUE EMAIL ----------------------------------------------
-use nutridb;
 
 IF OBJECT_ID ( 'UniqueEmail', 'P' ) IS NOT NULL
     DROP PROCEDURE UniqueEmail;
@@ -120,9 +195,112 @@ Create procedure [dbo].UniqueEmail
 	END
 
 GO
+-------------------------------------- UNIQUE PLAN NAME --------------------------------------------
 
+IF OBJECT_ID ( 'UniquePlanName', 'P' ) IS NOT NULL
+    DROP PROCEDURE UniquePlanName;
+GO
+
+Create procedure [dbo].UniquePlanName
+    (
+		@nombre varchar(max)
+    )
+   AS
+   BEGIN
+
+    DECLARE @temp varchar(max)
+    SET @temp = (
+        Select nombre
+        FROM Plans
+        WHERE nombre = @nombre
+	)
+
+    -- if the row to be inserted already exists, put the genreID into the @genreID output parameter
+
+    IF @temp IS NULL
+        BEGIN
+        SELECT CAST(1 AS bit) -- available
+        END
+
+    IF @temp IS NOT NULL
+        BEGIN
+        SELECT CAST(0 AS bit) -- unavailable
+        END
+
+	END
+
+GO
+---------------------------------------- UNIQUE PRODUCT DESCRIPTION -------------------------------
+
+IF OBJECT_ID ( 'UniqueProductDescription', 'P' ) IS NOT NULL
+    DROP PROCEDURE UniqueProductDescription;
+GO
+
+Create procedure [dbo].UniqueProductDescription
+    (
+		@description varchar(max)
+    )
+   AS
+   BEGIN
+
+    DECLARE @temp varchar(max)
+    SET @temp = (
+        Select descripcion
+        FROM Producto
+        WHERE descripcion = @description
+	)
+
+    -- if the row to be inserted already exists, put the genreID into the @genreID output parameter
+
+    IF @temp IS NULL
+        BEGIN
+        SELECT CAST(1 AS bit) -- available
+        END
+
+    IF @temp IS NOT NULL
+        BEGIN
+        SELECT CAST(0 AS bit) -- unavailable
+        END
+
+	END
+
+GO
+---------------------------------------- UNIQUE RECIPE NAME ---------------------------------------
+
+IF OBJECT_ID ( 'UniqueRecipeName', 'P' ) IS NOT NULL
+    DROP PROCEDURE UniqueRecipeName;
+GO
+
+Create procedure [dbo].UniqueRecipeName
+    (
+		@nombre varchar(max)
+    )
+   AS
+   BEGIN
+
+    DECLARE @temp varchar(max)
+    SET @temp = (
+        Select nombre
+        FROM Receta
+        WHERE nombre = @nombre
+	)
+
+    -- if the row to be inserted already exists, put the genreID into the @genreID output parameter
+
+    IF @temp IS NULL
+        BEGIN
+        SELECT CAST(1 AS bit) -- available
+        END
+
+    IF @temp IS NOT NULL
+        BEGIN
+        SELECT CAST(0 AS bit) -- unavailable
+        END
+
+	END
+
+GO
 ---------------------------------------- LOG IN ----------------------------------------------------
-USE [nutridb]
 
 IF OBJECT_ID('LogIn', 'P') IS NOT NULL
     DROP PROCEDURE [LogIn];
@@ -211,7 +389,6 @@ END
 GO
 
 --------------------------------------- REGISTER ----------------------------------------------
-USE [nutridb]
 
 IF OBJECT_ID('Register', 'P') IS NOT NULL
     DROP PROCEDURE [Register];
@@ -273,7 +450,6 @@ END
 GO
 
 ----------------------------------------- MASTER CLIENT -------------------------------------------------------
-USE [nutridb]
 
 IF OBJECT_ID('MasterClient', 'P') IS NOT NULL
     DROP PROCEDURE [MasterClient];
@@ -386,7 +562,6 @@ END
 GO
 
 ---------------------------------------------------- MASTER NUTRICIONIST -------------------------------------
-use nutridb;
 
 IF OBJECT_ID ( 'MasterNutricionist', 'P' ) IS NOT NULL
     DROP PROCEDURE [MasterNutricionist];
@@ -429,7 +604,6 @@ Create procedure [dbo].[MasterNutricionist]
 
 GO
 ------------------------------------------------ MASTER PRODUCT ---------------------------------------------------
-use nutridb;
 
 IF OBJECT_ID ( 'MasterProduct', 'P' ) IS NOT NULL
     DROP PROCEDURE [MasterProduct];
@@ -538,3 +712,183 @@ Create procedure [dbo].[MasterProduct]
 	END
 
 GO
+
+------------------------------------------- MASTER PLANS -----------------------------------------
+
+IF OBJECT_ID('MasterPlans', 'P') IS NOT NULL
+    DROP PROCEDURE [MasterPlans];
+GO
+
+Create procedure [dbo].[MasterPlans](
+    @id int = NULL,
+    @id_cliente int = NULL,
+    @id_nutricionista int = NULL,
+    @id_producto int = NULL,
+    @id_plan_cliente int = NULL,
+    @fecha Date = NULL,
+    @nombre varchar(20) = NULL,
+    @tiempo_comida varchar(20) = NULL,
+    @porciones float = NULL,
+    @estatus varchar(20) = 'ACTIVO',
+    @StatementType NVARCHAR(20) = ''
+)
+AS
+BEGIN
+
+    IF @StatementType = 'SelectAll'
+        BEGIN
+            select * from Plans where id_nutricionista = @id_nutricionista and estatus != 'INACTIVO'
+        END
+
+    IF @StatementType = 'SelectOne'
+        BEGIN
+            select *
+            from VistaProductosPlan
+            where id_plan = @id
+        END
+
+    IF @StatementType = 'InsertPlan'
+        BEGIN
+            insert into Plans (id_nutricionista, estatus, nombre)
+            values (@id_nutricionista, @estatus, @nombre)
+        END
+
+    IF @StatementType = 'InsertProductsPlan'
+        BEGIN
+            insert into Productos_plan (id_producto, id_plan, tiempo_comida, porciones)
+            values (@id_producto, @id, @tiempo_comida, @porciones)
+        END
+
+    IF @StatementType = 'DeletePlan'
+        BEGIN
+            update Plans
+            set estatus = 'INACTIVO'
+            where id = @id
+        END
+
+    IF @StatementType = 'DeletePlanProduct'
+    BEGIN
+        delete from Productos_plan where id_producto=@id_producto and id_plan=@id and tiempo_comida=@tiempo_comida
+    END
+
+
+END
+
+GO
+
+-------------------------------------------- MASTER RECIPE --------------------------------------
+
+IF OBJECT_ID('MasterRecipe', 'P') IS NOT NULL
+    DROP PROCEDURE [MasterRecipe];
+GO
+
+Create procedure dbo.[MasterRecipe](
+    @id int = NULL,
+    @id_cliente int = NULL,
+    @id_producto int = NULL,
+    @nombre VARCHAR(20) = NULL,
+    @estatus VARCHAR(20) = 'ACTIVO',
+    @porcion float = NULL,
+    @StatementType NVARCHAR(20) = ''
+)
+AS
+BEGIN
+
+    IF @StatementType = 'SelectAll'
+        BEGIN
+
+            SELECT DISTINCT R.id        as       id_receta,
+                            R.estatus   as       estado_receta,
+                            R.nombre    as       nombre_receta,
+                            P.id        as       id_producto,
+                            barcode,
+                            descripcion as       nombre_producto,
+                            porciones   as       porcion_agregada,
+                            tamano_porcion,
+--                             sodio,
+--                             grasa,
+--                             energia,
+--                             hierro,
+--                             calcio,
+--                             proteina,
+--                             vitamina,
+--                             carbohidratos
+
+                            (SELECT estadisticas
+                             FROM VistaPrettyProductos VP
+                             WHERE VP.id = P.id) [stats]
+
+            FROM Receta R
+                     JOIN Producto_receta PR ON R.id = PR.id_receta
+                     JOIN Producto P ON PR.id_producto = P.id
+            ORDER BY R.nombre
+        END
+
+    IF @StatementType = 'SelectOne'
+        BEGIN
+            SELECT DISTINCT R.id        as       id_receta,
+                            R.estatus   as       estado_receta,
+                            R.nombre    as       nombre_receta,
+                            P.id        as       id_producto,
+                            barcode,
+                            descripcion as       nombre_producto,
+                            porciones   as       porcion_agregada,
+                            tamano_porcion,
+--                             sodio,
+--                             grasa,
+--                             energia,
+--                             hierro,
+--                             calcio,
+--                             proteina,
+--                             vitamina,
+--                             carbohidratos
+
+                            (SELECT estadisticas
+                             FROM VistaPrettyProductos VP
+                             WHERE VP.id = P.id) [stats]
+
+            FROM Receta R
+                     JOIN Producto_receta PR ON R.id = PR.id_receta
+                     JOIN Producto P ON PR.id_producto = P.id
+            WHERE @id = R.id
+            ORDER BY R.nombre
+        END
+
+    IF @StatementType = 'Insert'
+        BEGIN
+            INSERT INTO Receta (id_cliente, estatus, nombre)
+            VALUES (@id_cliente, @estatus, @nombre);
+        END
+
+
+    IF @StatementType = 'Update'
+        BEGIN
+            UPDATE Receta
+            SET nombre = @nombre
+            WHERE id = @id
+        END
+
+    IF @StatementType = 'Delete'
+        BEGIN
+            UPDATE Receta
+            SET estatus = 'INACTIVO'
+            WHERE id = @id
+            -- IMPLEMENTAR TRIGGER QUE ELIMINE REFERENCIAS QUE TENGAN A RECETA ID.
+        END
+
+    IF @StatementType = 'AddProduct'
+        BEGIN
+            INSERT INTO Producto_receta (id_producto, id_receta, porciones)
+            VALUES (@id_producto, @id, @porcion);
+        END
+
+    IF @StatementType = 'RemoveProduct'
+        BEGIN
+            DELETE FROM Producto_receta
+            WHERE id_receta = @id AND id_producto = @id_producto
+        END
+
+END
+
+GO
+
