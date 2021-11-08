c5fbd1b80e374363e27efd5bdedde6977b3ac688
Heutlett <44324449+Heutlett@users.noreply.github.com>
2021-11-08 00:09:19 -0600

    Added. Se actualizan los scripts

--- scripts/RESUMEN TODOS/(4) vista2.sql
+++ scripts/RESUMEN TODOS/(4) vista2.sql
@@ -1,23 +1,3 @@
-create view VistaProductosPlan
-as
-select id_plan,
-       tiempo_comida,
-       porciones,
-       id_producto,
-       barcode,
-       descripcion,
-       tamano_porcion,
-       sodio,
-       grasa,
-       energia,
-       hierro,
-       calcio,
-       proteina,
-       vitamina,
-       carbohidratos
-from ((Plans join Productos_plan on Plans.id = Productos_plan.id_plan)
-         join Producto on Productos_plan.id_producto = Producto.id)
-
 create view VistaRecetaProductos
 as
 SELECT R.id           as id_receta,
